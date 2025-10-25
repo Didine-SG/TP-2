bd3e1d17cef4ad40294a6b4907cd1d0b0e6bcbc4
DidineSG <didinesupergaming007@gmail.com>
2025-10-25 20:47:44 +0100

    Fix image paths: correct file extensions and filenames

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import MonCompte from './pages/MonCompte';
 
 
 const mesBouquets = [
-  { id: 1, nom: 'Bouquet de Tunis', descr: "Un dosage parfait de jasmins et de tulipes, des couleurs éclatantes et du soleil toute l'année chez vous", image: '/images/bouquetTunis.jpg', prix: 1500.0 },
-  { id: 2, nom: 'Bouquet d\'Alger', descr: "Un mélange merveilleux de jasmins et de senteurs méditerranéennes, des odeurs éclatantes pour égayer votre bureau", image: '/images/bouqetAlger.jpg', prix: 2000.0 },
-  { id: 3, nom: 'Bouquet d\'Oran', descr: "Un mélange merveilleux de roses et de lys, des odeurs et des couleurs", image: '/images/bouquetOran.jpg', prix: 2000.0 }
+  { id: 1, nom: 'Bouquet de Tunis', descr: "Un dosage parfait de jasmins et de tulipes, des couleurs éclatantes et du soleil toute l'année chez vous", image: '/images/boquetTunis.png', prix: 1500.0 },
+  { id: 2, nom: 'Bouquet d\'Alger', descr: "Un mélange merveilleux de jasmins et de senteurs méditerranéennes, des odeurs éclatantes pour égayer votre bureau", image: '/images/bouqetAlger.png', prix: 2000.0 },
+  { id: 3, nom: 'Bouquet d\'Oran', descr: "Un mélange merveilleux de roses et de lys, des odeurs et des couleurs", image: '/images/boquetOran.png', prix: 2000.0 }
 ];
 
 
